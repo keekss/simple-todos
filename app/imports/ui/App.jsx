d2d16288d41fed210d8e230f8906ce440976bdba
keekss <keeksslss@gmail.com>
2020-10-18 23:18:48 -1000

    Section 3

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task';
+import { TaskForm } from './TaskForm';
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
 
   return (
       <div>
